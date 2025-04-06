745ad5bee0af8a24215bc1ac613f08aa4b24e73c
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-04-06 23:59:23 +0200

    main: update main file and CMakeLists.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 
+
+
+#define RUN_LINKED_LIST
+// #define RUN_HEAP
+// #define RUN_DYNAMIC_PROGRAMMING
+
+#ifdef RUN_LINKED_LIST
+#include "linked_list/linked_list.hpp"
+#endif
+
+#ifdef RUN_HEAP
+#include "heap/heap.hpp"
+#endif
+
+#ifdef RUN_DYNAMIC_PROGRAMMING
+#include "dynamic_programming/dp.hpp"
+#endif
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "=== Algorithm Playground ===\n";
+
+#ifdef RUN_LINKED_LIST
+    std::cout << "[Linked List Problems]\n";
+    LinkedList ll1;
+    LinkedList ll2(10);
+    std::cout << (ll1.isEmpty() ? "Empty\n" : "Not empty\n");
+    std::cout << (ll2.isEmpty() ? "Empty\n" : "Not empty\n");
+    ll2.append(3);
+    ll2.append(6);
+    ll2.append(3);
+    std::cout << (ll2.isEmpty() ? "Empty\n" : "Not empty\n");
+    ll2.print();
+#endif
     return 0;
 }
+
